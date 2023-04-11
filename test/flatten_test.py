94251211ef3619262d7172385993c92c1333d72f
KuntiPatidar <kuntipatidar1997@gmail.com>
2023-04-11 15:07:51 +0900

    made changes

--- test/flatten_test.py
+++ test/flatten_test.py
@@ -5,6 +5,9 @@ def read_file(filename):
     with open(filename, 'r') as f:
         data = json.load(f)
     return data
+# data=read_file("input/test3.json")
+# print(data)
+# print(simple_flatten_json.flatten_json(data))
 
 # Create a test case class
 class TestFlatClass(unittest.TestCase):
@@ -14,6 +17,17 @@ class TestFlatClass(unittest.TestCase):
         function_output=simple_flatten_json.flatten_json(data)
         correct_result=read_file("output/result1.json")
         self.assertEqual(function_output,correct_result)
+    def test_2(self):
+        data=read_file("input/test2.json")
+        function_output=simple_flatten_json.flatten_json(data)
+        correct_result=read_file("output/result2.json")
+        self.assertEqual(function_output,correct_result)
+    def test_3(self):
+        data=read_file("input/test3.json")
+        function_output=simple_flatten_json.flatten_json(data)
+        correct_result=read_file("output/result3.json")
+        self.assertEqual(function_output,correct_result)
+
 
 if __name__ == '__main__':
     unittest.main()
